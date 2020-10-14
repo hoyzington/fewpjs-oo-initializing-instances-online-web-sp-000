173d2dce36ac7a84b8dfb3452f642d85bb8f1095
Scott Hoisington <hoyzington@gmail.com>
2020-10-14 03:10:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ class Breakfast {
 
 class Lunch {
   constructor(salad, soup, drink) {
-    this. = ;
-    this. = ;
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
   }
 }
 
-class {
-  constructor(, ) {
-    this. = ;
-    this. = ;
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
   }
 }
